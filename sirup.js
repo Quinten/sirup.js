809d0f81f93fd5ad11f504259ea6e9c94d180102
Quinten Clause <quintenclause@hotmail.com>
2015-02-01 17:38:02 +0100

    Update sirup.js

--- sirup.js
+++ sirup.js
@@ -1,5 +1,9 @@
 var sirup = function (expression) {
-  var sirupObject = window.sirup;
-  sirupObject.nodes = document.querySelectorAll(expression);
+  var sirupObject = new sirup();
+  if (expression) {
+    sirupObject.nodes = document.querySelectorAll(expression);
+  } else {
+    sirupObject.nodes = null;
+  }
   return sirupObject;
 };
