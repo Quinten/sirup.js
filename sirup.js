a26691901e339cc77fa8f2951be7ca9d79ba5c8f
Quinten Clause <quintenclause@hotmail.com>
2015-02-01 20:56:03 +0100

    Update sirup.js

--- sirup.js
+++ sirup.js
@@ -24,16 +24,36 @@ var sirup = function (expression) {
     }
     return this;
   };
-  
-  this.add = function() {
-    alert('add');
-    return this;
-  }
 
-  this.del = function() {
-    alert('delete');
-    return this;
+  var class_list = !!document.body.classList;
+  var s = '(\\s|^)'; // space or start
+  var e = '(\\s|$)'; // space or end
+
+  function getRegex(className){
+    return new RegExp(s + className + e, 'g');
   }
+  
+  this.addClass = function() {
+    
+    return this;
+  };
+
+  this.removeClass = function() {
+    
+    return this;
+  };
+  
+  this.hasClass = function (className) {
+    if(!!document.body.classList){
+        return this.get[0].classList.contains(className);
+    }else{
+        return this.get[0].className.match(new RegExp('(\\s|^)' + className + '(\\s|^)', 'g'));
+    }
+  };
+  
+  this.toggleClass = function () {
+    return this;
+  };
 
   if (this instanceof sirup) {
     return this.sirup;
