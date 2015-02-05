65950066ad4dd12adc6ac7891c4aa4e8e653cae3
Quinten Clause <quintenclause@hotmail.com>
2015-02-05 22:42:48 +0100

    Manually minimisations

--- sirup.js
+++ sirup.js
@@ -43,60 +43,64 @@ this.ready = function (func) {
   return this;
 };
 
-this.addClass = function(className) {
+this.getRE = function (str) {
+  return new RegExp('(\\s|^)' + str + '(\\s|^)', 'g');
+}
+
+this.addClass = function(str) {
   var n = this.get();
   if(!!document.body.classList){
     for (var i = 0; i < n.length; i++) {
-      n[i].classList.add(className);
+      n[i].classList.add(str);
     }
   } else {
     for (var i = 0; i < n.length; i++) {
-      if (!n[i].className.match(new RegExp('(\\s|^)' + className + '(\\s|^)', 'g'))) {
-        n[i].className += ' ' + className;
+      if (!n[i].className.match(this.getRE(str))) {
+        n[i].className += ' ' + str;
       }
     }
   }
   return this;
 };
 
-this.removeClass = function(className) {
+this.removeClass = function(str) {
   var n = this.get();
   if(!!document.body.classList){
     for (var i = 0; i < n.length; i++) {
-      n[i].classList.remove(className);
+      n[i].classList.remove(str);
     }
   } else {
     for (var i = 0; i < n.length; i++) {
-      n[i].className = n[i].className.replace(new RegExp('(\\s|^)' + className + '(\\s|^)', 'g'), '');
+      n[i].className = n[i].className.replace(this.getRE(str), '');
     }
   }
   return this;
 };
 
-this.hasClass = function (className) {
+this.hasClass = function (str) {
   if(!!document.body.classList){
-      return this.get()[0].classList.contains(className);
+      return this.get()[0].classList.contains(str);
   }else{
-      return this.get()[0].className.match(new RegExp('(\\s|^)' + className + '(\\s|^)', 'g'));
+      return this.get()[0].className.match(this.getRE(str));
   }
 };
 
-this.toggleClass = function (className) {
+this.toggleClass = function (str) {
   var n = this.get();
   if(!!document.body.classList){
     for (var i = 0; i < n.length; i++) {
-      if (!n[i].classList.contains(className)) {
-        n[i].classList.add(className);
+      if (!n[i].classList.contains(str)) {
+        n[i].classList.add(str);
       } else {
-        n[i].classList.remove(className);
+        n[i].classList.remove(str);
       }
     }
   } else {
     for (var i = 0; i < n.length; i++) {
-      if (!n[i].className.match(new RegExp('(\\s|^)' + className + '(\\s|^)', 'g'))) {
+      if (!n[i].className.match(this.getRE(str))) {
         n[i].className += ' ' + className;
       } else {
-        n[i].className = n[i].className.replace(new RegExp('(\\s|^)' + className + '(\\s|^)', 'g'), '');
+        n[i].className = n[i].className.replace(this.getRE(str), '');
       }
     }
   }
